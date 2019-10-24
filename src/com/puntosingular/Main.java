2d54e55ccbe46eb63560f04c651e4f059efb94c8
Sandra Aguilera <siar011099@gmail.com>
2019-10-23 19:11:37 -0500

    Agregado archivo base del convertidor

---
+++ src/com/puntosingular/Main.java
@@ -0,0 +1,9 @@
+package com.puntosingular;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("Convertidor de números arábigos a números romanos");
+    }
+}
